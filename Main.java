9bdd9f89acfa5e6564973fdf1f0f45c97430b468
vaalss <valeria.hernandezmaldo@gmail.com>
2026-03-18 12:38:41 -0600

    arreglo a main

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
+
     public static List<String> readFile(String filePath) throws IOException {
         return Files.readAllLines(Path.of(filePath));
     }
@@ -11,14 +13,21 @@ public class Main {
     public static void main(String[] args) {
         try {
             List<String> lines = readFile("text.txt");
+
+            StringBuilder sb = new StringBuilder();
+
             for (String line : lines) {
-                System.out.println(lines.toString());
+                sb.append(line).append(" ");
             }
+
+            String textoCompleto = sb.toString();
+            
+            Map<Character, List<Object>> tabla = Conteo.contarCaracteres(textoCompleto);
+
+
         } catch (IOException e) {
             System.err.println("error leyendo el archivo");
         }
     }
-
-
 }
 
